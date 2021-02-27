58c089369b375fba1b1b2ec90713de4a52b07939
harissmajlovic <smajlovicharis@hotmail.com>
2021-02-27 12:01:34 -0800

    Comments added

--- sw_simded_alpern.cpp
+++ sw_simded_alpern.cpp
@@ -15,24 +15,31 @@ template < typename T >
         unsigned int quantity = sequences.size();
         std::vector<std::vector<__m256i>> matrix(size + 1, std::vector<__m256i>(size + 1, _mm256_setzero_si256()));
 
+        // Instantiate SIMDed scores
         const __m256i gap      = _mm256_set1_epi32(-2);
         const __m256i mismatch =  _mm256_set1_epi32(-2);
         const __m256i match    =  _mm256_set1_epi32(3);
+        
+        // SIMD size
         unsigned int sse_s     = 8;
 
         for (unsigned int k = 0; k < quantity; k += sse_s) {
+            // Target SIMDed values
             __m256i max_element   = _mm256_setzero_si256();
-            __m256i max_element_i = _mm256_setzero_si256();
-            __m256i max_element_j = _mm256_setzero_si256();
+            // __m256i max_element_i = _mm256_setzero_si256();
+            // __m256i max_element_j = _mm256_setzero_si256();
+            
+            // Auxiliary values
             __m256i diagonal_value, top_value, left_value,
                     top_left, top_left_shifted, temp, diagonal_temp,
                     diagonal_temp_shifted, target_value, target_value_shifted,
                     top_left_and, diagonal_temp_and, target_value_and,
                     max_target_shifted, max_target;
 
+            // Construct next SIMD sized batch of chars
             std::vector<char*> i_seq(size, new char[sse_s]);
             std::vector<char*> j_seq(size, new char[sse_s]);
-
+            // Construct next SIMD sized batch of chars
             for (unsigned int p = 0; p < sse_s; ++p) {
                 for (unsigned int i = 0; i < size; ++i) {
                     i_seq[i][p] = sequences[k + p].first[i];
@@ -42,28 +49,34 @@ template < typename T >
             
             for (unsigned int i = 1; i < size + 1; ++i){
                 for(unsigned int j = 1; j < size + 1; ++j) {
+                    // match_val ~ data.first[i - 1] == data.second[j - 1] ? match : mismatch)
                     // long int mask = atoi(i_seq[i]) ^ atoi(j_seq[j]);
                     const int mask = 19;
                     __m256i match_val = _mm256_blend_epi32(mismatch, match, mask);  // (sequences[k].first[i - 1] == sequences[k].second[j - 1] ? match : mismatch);
                     
+                    // diagonal_value ~ matrix[i-1][j-1] + match_val
                     diagonal_value   = _mm256_add_epi32 ( matrix[i-1][j - 1], match_val );
+                    // top_value ~ matrix[i-1][j] + gap
                     top_value        = _mm256_add_epi32 ( matrix[i-1][j], gap);
+                    // left_value ~ matrix[i][j-1] + gap
                     left_value       = _mm256_add_epi32 ( matrix[i][j - 1], gap) ;
+                    
+                    // Calculate target_value ~ std::max(diagonal_value, std::max(top_value, left_value))
                     top_left         = _mm256_sub_epi32 ( top_value, left_value );
                     top_left_shifted = _mm256_srli_epi32( top_left,  31 );
                     top_left_and     = top_left & top_left_shifted;
                     temp             = _mm256_sub_epi32 ( top_value, top_left_and );
                     diagonal_temp    = _mm256_sub_epi32 ( diagonal_value, temp );
-                    
                     diagonal_temp_and = diagonal_temp & diagonal_temp_shifted;
-                    
                     diagonal_temp_shifted  = _mm256_srli_epi32( diagonal_temp, 31 );
                     target_value           = _mm256_sub_epi32 ( diagonal_value, diagonal_temp_and );
+
+                    // Calculate  matrix[i][j] ~ (target_value > 0) ? target_value : 0
                     target_value_shifted   = _mm256_srli_epi32( target_value, 31 );
-                    
                     target_value_and       = target_value & target_value_shifted;
                     matrix[i][j]           = _mm256_sub_epi32 ( target_value, target_value_and );
 
+                    // Update max_element and coordinates if the target_value is larger
                     max_target         = _mm256_sub_epi32(target_value, max_element);
                     max_target_shifted = _mm256_srli_epi32(max_target, 31);
                     max_element        = _mm256_sub_epi32(target_value, (max_target & max_target_shifted));
